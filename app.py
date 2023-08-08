16e1bbaf3c2829280ec6cb4fb327da2a63e2c3d7
Tekabe Fanta <tekabedesta.com>
2023-08-07 19:57:00 -0700

    some change

--- app.py
+++ app.py
@@ -11,17 +11,30 @@ engine = db.create_engine("database path")
 conn = engine.connect()
 session = Session(bind=engine)
 
+
 @app.route("api/v1/states")
 # this api should return distinct states as list
+def states():
+    states
 
-@app.route("/api/v1/<state>/cities") 
+
+@app.route("/api/v1/<state>/cities")
 #  this api should return all the cities in provided state
+def cities():
+    cities
+
 
 @app.route("/api/v1/<state>/<city>/cuisines")
 #  this api should return all the cuisines in that city
+def cuisines():
+    cuisines
+
 
 @app.route("/api/v1/<state>/<city>/<cuisine>/restaurants")
 #  this api should return all the restaurants for selected cuisine.
+def restaurants():
+    restaurants
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
