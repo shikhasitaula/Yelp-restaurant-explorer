eadf929526c9c45db6a873ae8859e651059e2c06
vidhya67meg <mvidhya.ft@gmail.com>
2023-08-09 16:56:22 -0700

    more routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, desc
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import numpy as np
@@ -34,6 +34,7 @@ def index():
         f"Available Routes:<br/>"
         f"/api/v1.0/cuisines<br/>"
         f"/api/v1.0/restaurant_info<br/>"
+        f"/api/v1.0/cuisine_distribution<br/>"
         f"/api/v1.0/states"
     )
 
@@ -61,7 +62,7 @@ def states():
 def cuisines():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    # Find the most recent date in the data set.
+    # Find the unique cuisines in the data set.
     unique_cuisines = session.query(Cuisine.cuisines).distinct().all()
     session.close()
     cuisine_list = []
@@ -79,7 +80,7 @@ def location():
     
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    # Find the most recent date in the data set.
+    # Joining cuisine with restaurant table and filtering data.
     query = session.query(Metadata)
     if cuisine:
         query=query.join(Cuisine, Metadata.id==Cuisine.id).filter(
@@ -114,6 +115,90 @@ def location():
     
     return jsonify(restaurants_list)
 
+@app.route("/api/v1.0/cuisine_distribution")
+def cuisine_distribution():
+    state = request.args.get('state', type=str)
+    price = request.args.get('price',type=str)
+    rating = request.args.get('rating',type=float)
+ 
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Joining cuisine and metadata tables and getting cuisines of restaurants.
+    query = session.query(Cuisine.cuisines.label('cuisine'),func.count(Metadata.id).label('count')).join(
+        Cuisine, Metadata.id==Cuisine.id
+    )
+    if state:
+        query=query.filter(Metadata.state==state)
+    if price:
+        query=query.filter(Metadata.price==price)
+    if rating:
+        query=query.filter(Metadata.rating>=rating)
+    query = query.group_by(Cuisine.cuisines).order_by(desc('count'))
+    
+    
+    cuisine_group = query.all()
+
+    # query to get total count
+    query = session.query(func.count(Metadata.id).label('total'))
+    if state:
+        query=query.filter(Metadata.state==state)
+    if price:
+        query=query.filter(Metadata.price==price)
+    if rating:
+        query=query.filter(Metadata.rating>=rating)
+    
+    total = query.one()[0]
+    print(total)
+    
+    session.close()
+    cuisine_group_list = []
+    for c in cuisine_group:
+        cuisine_dict = {
+           "cuisine": c.cuisine,
+            "count": c.count,
+            "percentage": round((c.count / total) * 100, 1 )
+        }
+        cuisine_group_list.append(cuisine_dict)
+
+       
+    
+    return jsonify(cuisine_group_list)
+
+@app.route("/api/v1.0/price_rating")
+def price_rating():
+    state = request.args.get('state', type=str)
+    cuisine = request.args.get('cuisine', type=str)
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Getting price and ratings
+    query = session.query(Metadata.price,Metadata.rating,func.count(Metadata.id).label('count'))
+
+    if cuisine:
+        query=query.join(Cuisine, Metadata.id==Cuisine.id).filter(
+            Cuisine.cuisines==cuisine
+            )
+    if state:
+        query=query.filter(Metadata.state==state)
+    
+    query = query.group_by(Metadata.price,Metadata.rating).order_by(Metadata.price)
+
+    result = query.all()
+    
+    session.close()
+    price_rating_list = []
+    for pr in result:
+        cuisine_dict = {
+           "price": "Unknown" if pr.price==None else pr.price,
+           "rating": pr.rating,
+           "count": pr.count,
+        }
+        price_rating_list.append(cuisine_dict)
+
+       
+    
+    return jsonify(price_rating_list)
 
 
 
